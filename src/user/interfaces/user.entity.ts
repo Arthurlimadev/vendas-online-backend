d383e85c7e64f7017e89d6d5fa18f9d1de43d2c5
Arthur Lima <arthurlimadev98@gmail.com>
2023-05-02 06:27:23 -0300

    create migration

--- src/user/interfaces/user.entity.ts
+++ src/user/interfaces/user.entity.ts
@@ -4,19 +4,19 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 export class UserEntity {
   @PrimaryGeneratedColumn('rowid')
   id: number;
-  
-  @Column({name: 'name', nullable: false})
+
+  @Column({ name: 'name', nullable: false })
   name: string;
- 
-  @Column({name: 'email', nullable: false})
+
+  @Column({ name: 'email', nullable: false })
   email: string;
 
-  @Column({name: 'phone', nullable: false})
+  @Column({ name: 'phone', nullable: false })
   phone: string;
 
-  @Column({name: 'cpf', nullable: false})
+  @Column({ name: 'cpf', nullable: false })
   cpf: string;
 
-  @Column({name: 'password', nullable: false})
+  @Column({ name: 'password', nullable: false })
   password: string;
 }
