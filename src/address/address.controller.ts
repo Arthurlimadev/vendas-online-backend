06f4b78b1c343845424c6d8e10bc629c40edaf79
Arthur Lima <arthurlimadev98@gmail.com>
2023-05-06 19:53:38 -0300

    adresses

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CreateAddressDto } from './dto/createAddress.dto';
 import { AddressService } from './address.service';
+import { AddressEntity } from './entities/address.entity';
 
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
+
+  @Post(':/userId')
+  @UsePipes(ValidationPipe)
+  async createAddress(
+    createAddressDto: CreateAddressDto,
+  ): Promise<AddressEntity> {
+    return this.addressService.createAddress(createAddressDto);
+  }
 }
